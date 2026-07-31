477efee3528368052d8d328cadf561e7fe795cf7
nicholasmerritt-acc <nicholas.merritt@g.austincc.edu>
2026-07-31 08:57:54 -0500

    drop items when we break a crate
    for now, small equal chance at each item from a crate.
    we allow null items because we want a chance at no loot

--- CakeFlip/Assets/Scripts/ItemSpawner.cs
+++ CakeFlip/Assets/Scripts/ItemSpawner.cs
@@ -9,17 +9,26 @@ public class ItemSpawner : MonoBehaviour
     [SerializeField] private Transform spawnPosition;
     [SerializeField] private GameObject[] spawnPrefabs;
     [SerializeField] private int[] weights;
+    [SerializeField] private bool continuousSpawning = true;
 
     void Start()
     {
-        StartCoroutine(nameof(SpawnItem));
+        if (spawnPosition == null)
+        {
+            spawnPosition = transform;
+        }
+
+        if (continuousSpawning)
+        {
+            StartCoroutine(nameof(SpawnLoop));
+        }
     }
 
     /// <summary>
     /// Instantiate a prefab, after some initial delay, every spawnInterval seconds at our designated location
     /// </summary>
     /// <returns></returns>
-    private IEnumerator SpawnItem()
+    private IEnumerator SpawnLoop()
     {
         if (spawnPrefabs == null || spawnPrefabs.Length == 0 || weights == null || weights.Length == 0 || spawnPrefabs.Length != weights.Length)
         {
@@ -32,8 +41,21 @@ public class ItemSpawner : MonoBehaviour
         while (true)
         {
             yield return new WaitForSeconds(spawnInterval);
-            Instantiate(spawnPrefabs.GetWeightedItem(weights), spawnPosition.position, Quaternion.identity);
+            SpawnOnce();
         }
+    }
+
+    /// <summary>
+    /// Spawn an item from our list of items
+    /// </summary>
+    public GameObject SpawnOnce()
+    {
+        GameObject spawnMe = spawnPrefabs.GetWeightedItem(weights);
+        if (spawnMe != null)
+        {
+            spawnMe = Instantiate(spawnMe, spawnPosition.position, spawnMe.transform.rotation);
+        }
+        return spawnMe;
     }
 
     private void OnDisable()
--- CakeFlip/Assets/Scripts/Items/ExplodableObject.cs
+++ CakeFlip/Assets/Scripts/Items/ExplodableObject.cs
@@ -6,6 +6,7 @@ public class ExplodableObject : MonoBehaviour
     [SerializeField] private bool bigExplosion = false;
     [SerializeField] private float explosionRadius = 1f;
     [SerializeField] private float explosionForce = 10f;
+    [SerializeField] private bool spawnOnDeath = false;
 
     private void OnCollisionEnter(Collision collision)
     {
@@ -14,6 +15,17 @@ public class ExplodableObject : MonoBehaviour
             //explode when we collide with skateboard
             if (collision.gameObject.GetComponent<PlayerController>().IsSkateboard) {
                 Instantiate(explosionPrefab, transform.position, Quaternion.identity);
+
+                if (spawnOnDeath)
+                {
+                    if (TryGetComponent<ItemSpawner>(out ItemSpawner spawner))
+                    {
+                        spawner.SpawnOnce();
+                    }
+                }
+
+                //add explosion force to all colliders around this.
+                //we do this after spawning so we have a chance of launching some item into the air
                 if (bigExplosion)
                 {
                     Vector3 explosionPosition = transform.position;
